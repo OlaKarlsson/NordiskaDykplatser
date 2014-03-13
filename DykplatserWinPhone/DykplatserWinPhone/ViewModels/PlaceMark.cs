5131dcdc5ed4327bb7c7aca88328d5c113ec155a
Ola Karlsson <ola@olakarlsson.com>
2014-03-13 20:53:41 +0100

    Initial add of code

---
+++ DykplatserWinPhone/DykplatserWinPhone/ViewModels/PlaceMark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DykplatserWinPhone.ViewModels
+{
+    public class PlaceMark
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
